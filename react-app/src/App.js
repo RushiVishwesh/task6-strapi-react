669481b4fcaa97f5df3c18bd4df2cefc0fd4c613
Vishwesh Rushi <129622241+RushiVishwesh@users.noreply.github.com>
2024-07-12 01:24:05 +0530

    test

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,14 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    message: '',
+  };
+
+  componentDidMount() {
+    axios.get('https://ansal-sajan-api.contentecho.in/api/messages')
+      .then(response => {
+        if (response.data && response.data.data && response.data.data.length > 0) {
+          this.setState({ message: response.data.data[0].attributes.text });
+        }
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello world!</h1>
+          <h1 className="App-title">{this.state.message}</h1>
         </header>
         <p className="App-intro">
           Looks like you're all set!
